1658c1094725af3444dd062b4368203aff65e021
bryan-clark <brayanraherinandrasana@gmail.com>
2024-03-01 12:03:10 +0300

    ...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,6 +44,7 @@ class HomeController extends Controller
             "Friday" => "Vendredi",
             "Saturday" => "Samedi"
         ];
+        $myRecord = [];
         $record = Etudiant::select(DB::raw("COUNT(*) as count"), DB::raw("DAYNAME(created_at) as day_name"), DB::raw("DAY(created_at) as day"))->where('created_at', '>', Carbon::today()->subDay(6))
             ->groupBy('day_name', 'day')
             ->orderBy('day')
